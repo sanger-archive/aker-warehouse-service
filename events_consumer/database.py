22a4094edbe7a92c28220412faf2be1569256697
David Robinson <dr6@sanger.ac.uk>
2017-06-06 10:36:17 +0100

    Added code to read staging/production database details from config file

--- events_consumer/database.py
+++ events_consumer/database.py
@@ -1,13 +1,14 @@
+from ConfigParser import ConfigParser
+
 def setup_database(env='development'):
     if env in ('development', 'test'):
         import sqlite3
         conn = sqlite3.connect(':memory:')
         create_schema(conn)
-    elif env=='staging':
+    elif env in ('staging', 'production'):
         import psycopg2
-        conn = psycopg2.connect(TODO('connection details'))
-    elif env=='production':
-        TODO # prod database
+        details = connection_details(env)
+        conn = psycopg2.connect(**details)
     else:
         raise ValueError("Unrecognised environment: %r"%env)
     return conn
@@ -16,3 +17,16 @@ def setup_database(env='development'):
 def create_schema(conn):
     with open('schema.sql', 'r') as fin:
         conn.executescript(fin.read())
+
+def connection_details(env):
+    filename = '%s_db.txt'%env
+    config = ConfigParser()
+    config.read(filename)
+    values = config.defaults()
+    return {
+        'host': values['host'],
+        'port': int(values['port']),
+        'database': values['database'],
+        'user': values['user'],
+        'password': values['password'],
+    }
